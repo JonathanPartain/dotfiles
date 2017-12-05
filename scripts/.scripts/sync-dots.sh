dbf3b01116d950ede21bbcc3773926481d47d4af
JonathanPartain <jonathan.partain@gmail.com>
2017-12-05 15:37:47 +0100

    Cron-push

--- scripts/.scripts/sync-dots.sh
+++ scripts/.scripts/sync-dots.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
-echo "Synching i3 directory... 1/5"
-rsync -av /home/jonathan/.config/i3/ /home/jonathan/dotfiles/home/.config/i3/
 
-echo "Synching polybar directory... 2/5"
-rsync -av /home/jonathan/.config/polybar/ /home/jonathan/dotfiles/home/.config/polybar/
+cd /home/jonathan/dotfiles
 
-echo "Synching scripts directory... 3/5"
-rsync -av /home/jonathan/.scripts/ /home/jonathan/dotfiles/home/.scripts/
+git pull origin master
 
-echo "Synching vimrc file... 4/5"
-rsync -av /home/jonathan/.vimrc /home/jonathan/dotfiles/home/.vimrc
-
-echo "Synching bashrc file... 5/5"
-rsync -av /home/jonathan/.bashrc /home/jonathan/dotfiles/home/.bashrc
+stow */
 
 echo "Pushing to git"
-cd /home/jonathan/dotfiles
 git add .
 git commit -m "Cron-push"
 git push origin master
