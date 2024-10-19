56f25b3fbc0c41541d4a7a144d358a88aaf75477
Jonathan Partain <jonathan.partain@protonmail.com>
2024-10-19 15:42:33 +0200

    Updated install requirements, fixed readme

--- README.md
+++ README.md
@@ -1,18 +1,7 @@
 # Dotfiles
 
-Make sure that the following is installed:
-  * oh-my-zsh (make sure to set the correct shell as well)
-  * stow
-  * i3
-  * polybar
-  * rofi
-  * vim awesome configuration (https://github.com/amix/vimrc)
-  
 Clone the directory into `~/dotfiles`
 
-Move all configuration files into a temporary directory as a security measure. Alternatively, run `move-old-config.sh` inside `~/dotfiles/scripts/.scripts` directory
-
-Run `sync-dots.sh` inside `~/dotfiles/scripts/.scripts` directory
+run `install_requirements.sh`
 
 All should be ok!
-
--- install_requirements.sh
+++ install_requirements.sh
@@ -2,20 +2,34 @@
 
 #sudo apt update
 #
-#sudo apt install -y git
-#sudo apt install -y stow # needed for dotfile management
-#sudo apt install -y tmux
-#sudo apt install -y ripgrep
-#sudo apt install -y wget tar
+sudo apt install -y git
+sudo apt install -y stow # needed for dotfile management
+sudo apt install -y tmux
+sudo apt install -y ripgrep
+sudo apt install -y wget tar curl
+# clipboard stuff for x11 and wayland
+sudo apt install xclip wl-clipboard
+# From fixit github
 echo "deb [arch=$(dpkg --print-architecture) trusted=yes] https://eugene-babichenko.github.io/fixit/ppa ./" | sudo tee /etc/apt/sources.list.d/fixit.list > /dev/null
 sudo apt update
 sudo apt install fixit
+# install node and npm
+curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt install -y nodejs
+# Chown npm and node modules
+sudo chown -R $USER ~/.npm
+sudo chown -R $USER /usr/lib/node_modules/
+
+# install tldr
+sudo npm install -g tldr
+
 
 # Install nvim in Documents because why not
 # install nvim from latest stable, symlink to /usr/local/bin
 NVIM_VERSION=$(git ls-remote --refs --tags git@github.com:neovim/neovim.git | cut --delimiter='/' --fields=3 | tr '-' '~' | sort --version-sort | tail --lines=1)
-wget https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux64.tar.gz
-tar xf ~/Downloads/nvim-linux64.tar.gz --directory=~/Documents/
+wget https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux64.tar.gz -O ~/Downloads/nvim-linux64.tar.gz
+tar xf ~/Downloads/nvim-linux64.tar.gz --directory=~/Documents/nvim-linux64/
 # symlink to /usr/local/bin/
 sudo ln -s ~/Documents/nvim-linux64/bin/nvim /usr/local/bin/nvim
 
